6444ef333346d8e89405e05c1642d9b039a19652
Haru <haru@wrtn.io>
2024-12-09 23:28:46 +0900

    fix

--- site.config.js
+++ site.config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   },
 
   // CONFIG configration (required)
-  link: "https://myblog-phi-khaki.vercel.app",
+  link: "https://dongchang.vercel.app",
   since: 2024, // If leave this empty, current year will be used.
   lang: "ko-KR", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
@@ -70,7 +70,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 60, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
