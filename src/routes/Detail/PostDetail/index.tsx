a298d07d28bacedf2e02885e9036516c4f314a8d
dc.lee <dc.lee@toss.im>
2025-02-17 12:06:23 +0900

    fix design

--- src/routes/Detail/PostDetail/index.tsx
+++ src/routes/Detail/PostDetail/index.tsx
@@ -44,16 +44,14 @@ const PostDetail: React.FC<Props> = () => {
 export default PostDetail
 
 const StyledWrapper = styled.div`
-  padding-left: 1.5rem;
-  padding-right: 1.5rem;
-  padding-top: 3rem;
-  padding-bottom: 3rem;
+  padding: 1.5rem;
+
+  @media (max-width: 500px) {
+    padding: 0;
+  }
+
   border-radius: 1.5rem;
   max-width: 56rem;
-  background-color: ${({ theme }) =>
-    theme.scheme === "light" ? "white" : theme.colors.gray4};
-  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
-    0 2px 4px -1px rgba(0, 0, 0, 0.06);
   margin: 0 auto;
   > article {
     margin: 0 auto;
